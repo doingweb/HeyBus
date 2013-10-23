e6e80880f4bffb8df87518008facf26e0839eb55
Chris Antes <nicenecktie@gmail.com>
2013-10-23 12:56:40 -0700

    Initial - Yeoman generated

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('HeyBusApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
